458f61457412dfc8633eeebf7575c810f77be443
inecalvimontes <ines.calvimontes@uc.cl>
2022-04-23 15:47:17 -0400

    mensaje

---
+++ aplicación.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var div1 = ("#d1");
+
+    div1.click(function(){
+        $(this).show()
+    });
+
+    div1.click(function(){
+        $(this).hide()
+    });
+})
